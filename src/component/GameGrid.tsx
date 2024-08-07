fad220ed81ff5c1209c771b301caaec04eccb3b4
Jon-Ian A. Batomalaque <jonab@stpeter.com.ph>
2024-08-07 09:00:26 +0800

    Refractor: extract a query object

--- src/component/GameGrid.tsx
+++ src/component/GameGrid.tsx
@@ -4,14 +4,14 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
+import { GameQuery } from "../App";
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatForm: Platform | null;
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({ selectedGenre, selectedPlatForm }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatForm);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
