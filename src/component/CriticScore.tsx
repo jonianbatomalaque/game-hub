40e5fc0a5aaf6056e49658e5db50398a3a66e561
Jon-Ian A. Batomalaque <jonab@stpeter.com.ph>
2024-08-08 09:24:19 +0800

    test commit

--- src/component/CriticScore.tsx
+++ src/component/CriticScore.tsx
@@ -9,7 +9,7 @@ const CriticScore = ({ score }: Props) => {
   let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
 
   return (
-    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
+    <Badge colorScheme={color} fontSize="15px" paddingX={2} borderRadius="4px">
       {score}
     </Badge>
   );
